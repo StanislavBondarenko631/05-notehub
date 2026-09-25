12f85c360ea4a8b2bb37747aec33733507a711da
StanislavBondarenko631 <skyritm201287@gmail.com>
2026-09-25 13:07:42 +0300

    add code in NoteList comp.

---
+++ src/components/NoteList/NoteList.tsx
@@ -0,0 +1,38 @@
+import { useQuery } from "@tanstack/react-query";
+import styles from "./NoteList.module.css";
+import { fetchNotes } from "../../services/noteService";
+
+export default function NoteList() {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["notes", { page: 1, search: "" }],
+    queryFn: () => fetchNotes({ page: 1, search: "" }),
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error loading notes</div>;
+  }
+
+  const notes = data?.notes || [];
+
+  if (notes.length === 0) {
+    return null;
+  }
+
+  return (
+    <ul className={styles.list}>
+      {notes.map((note) => (
+        <li className={styles.listItem} key={note.id}>
+          <h2 className={styles.title}>{note.title}</h2>
+          <p className={styles.content}>{note.content}</p>
+          <div className={styles.footer}>
+            <span className={styles.tag}>{note.tag}</span>
+            <button className={styles.button}>Delete</button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
