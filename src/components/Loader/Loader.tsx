5ed5bb11b63f27032c581def570b042edb4a9c69
StanislavBondarenko631 <skyritm201287@gmail.com>
2026-09-25 21:01:48 +0300

    fixed bugs

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,5 +1,10 @@
 import css from "./Loader.module.css";
 
 export default function Loader() {
-  return <div className={css.container}>Loading notes, please wait...</div>;
+  return (
+    <div className={css.container}>
+      <div className={css.spinner}></div>
+      <p>Loading notes, please wait...</p>
+    </div>
+  );
 }
